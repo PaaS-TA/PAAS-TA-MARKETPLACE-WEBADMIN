03ae9660492ed76197c480d37824bfd15a98f653
hrjin <hyunlee@bluedigm.com>
2019-08-01 14:29:59 +0900

    security 추가 및 로그인 예제(추후 수정)

--- src/main/java/org/openpaas/paasta/marketplace/web/admin/common/PropertyService.java
+++ src/main/java/org/openpaas/paasta/marketplace/web/admin/common/PropertyService.java
@@ -10,48 +10,13 @@ import lombok.Data;
 public class PropertyService {
 
 	// market
-	@Value("${market.place.api.uri}")
+	@Value("${marketplace.api.url}")
     private String marketApiUri;
 	
-    @Value("${market.place.api.authorization.username}")
-    private String marketApiUsername;
-
-    @Value("${market.place.api.authorization.password}")
-    private String marketApiPassword;
-    
-
-	// PaaS-TA CF
-    @Value("${cf.java.client.api.uri}")
-    private String cfJavaClientApiUri;
-
-    @Value("${cf.java.client.api.authorization.username}")
-    private String cfJavaClientApiUsername;
-
-    @Value("${cf.java.client.api.authorization.password}")
-    private String cfJavaClientApiPassword;
-
-    @Value("${cf.api.url}")
-    private String cfApiUrl;
-
-    @Value("${cf.uaa.oauth.info.uri}")
-    private String cfUaaUserInfo;
-
-    @Value("${cf.uaa.oauth.token.check.uri}")
-    private String cfCheckTokenUri;
-
-    @Value("${cf.uaa.oauth.token.access.uri}")
-    private String cfAccessUri;
-
-    @Value("${cf.uaa.oauth.logout.uri}")
-    private String cfLogoutUri;
-
-    @Value("${cf.uaa.oauth.authorization.uri}")
-    private String cfAuthorizationUri;
-
-    @Value("${cf.uaa.oauth.client.id}")
-    private String cfClientId;
-
-    @Value("${cf.uaa.oauth.client.secret}")
-    private String cfClientSecret;
+//    @Value("${market.place.api.authorization.username}")
+//    private String marketApiUsername;
+//
+//    @Value("${market.place.api.authorization.password}")
+//    private String marketApiPassword;
 
 }

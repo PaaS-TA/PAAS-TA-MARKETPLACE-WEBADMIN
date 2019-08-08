7bc1cdcee3067d658b92493f4cd36a010e844d19
hrjin <hyunlee@bluedigm.com>
2019-08-08 20:31:40 +0900

    카테고리 관리 수정 중

--- src/main/java/org/openpaas/paasta/marketplace/web/admin/controller/CategoryController.java
+++ src/main/java/org/openpaas/paasta/marketplace/web/admin/controller/CategoryController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.openpaas.paasta.marketplace.web.admin.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -23,10 +24,40 @@ public class CategoryController {
      * @return ModelAndView(Spring 클래스)
      */
     @GetMapping
-    public String getCategoryMain() {
-        return "contents/categoryMain";
+    public String getCategoryListMain(Model model) {
+        model.addAttribute("categories", categoryService.getCategoryList());
+        return "contents/category-list";
+    }
+
+//    @GetMapping
+//    @ResponseBody
+//    public List<Category> getCategoryList() {
+//        return categoryService.getCategoryList();
+//    }
+
+
+    /**
+     * 카테고리 등록 페이지로 이동한다.
+     *
+     * @return
+     */
+    @GetMapping("/create")
+    public String createCategoryMain(){
+        return "contents/category-create";
     }
 
 
+    /**
+     * 카테고리 상세 조회한다.
+     *
+     * @param model
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "/{id}")
+    public String getCategoryMain(Model model, @PathVariable("id") Long id){
+        model.addAttribute("category", categoryService.getCategory(id));
+        return "contents/category-detail";
+    }
 
 }
--- src/main/java/org/openpaas/paasta/marketplace/web/admin/service/CategoryService.java
+++ src/main/java/org/openpaas/paasta/marketplace/web/admin/service/CategoryService.java
@@ -1,8 +1,30 @@
 package org.openpaas.paasta.marketplace.web.admin.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.openpaas.paasta.marketplace.api.domain.Category;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.List;
+
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class CategoryService {
+    private final RestTemplate paasApiRest;
 
+    public List<Category> getCategoryList() {
+        return paasApiRest.getForObject("/admin/categories?sort=seq,asc", List.class);
+    }
+
+    public Category getCategory(Long id) {
+        String url = UriComponentsBuilder.newInstance().path("/admin/categories/{id}")
+                .build()
+                .expand(id)
+                .toString();
+
+        return paasApiRest.getForObject(url, Category.class);
+    }
 }

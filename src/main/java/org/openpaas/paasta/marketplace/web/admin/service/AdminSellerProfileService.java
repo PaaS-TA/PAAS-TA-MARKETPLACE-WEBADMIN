1ec714345d4cb6642afbf6adc796e788bf4ce85f
sjchoi <sjchoi@bluedigm.com>
2019-08-26 16:43:15 +0900

    등록일/승인일 검색조건 추가

--- src/main/java/org/openpaas/paasta/marketplace/web/admin/service/AdminSellerProfileService.java
+++ src/main/java/org/openpaas/paasta/marketplace/web/admin/service/AdminSellerProfileService.java
@@ -3,11 +3,8 @@ package org.openpaas.paasta.marketplace.web.admin.service;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.openpaas.paasta.marketplace.api.domain.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.data.domain.Page;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -15,7 +12,6 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.transaction.Transactional;
-import java.util.List;
 import java.util.Map;
 
 @Service
@@ -27,16 +23,16 @@ public class AdminSellerProfileService {
     private final RestTemplate paasApiRest;
 
     @SneakyThrows
-    public List getProfiles() {
+    public Map getProfiles() {
         Map map  = paasApiRest.getForObject("/admin/profiles/page", Map.class);
         System.out.println(map.toString());
-        return null;
+        return map;
     }
 
     //Page::Profile
     public CustomPage<Profile> getProfileList(String queryParamString) {
         ResponseEntity<CustomPage<Profile>> responseEntity = paasApiRest.exchange("/admin/profiles/page" + queryParamString, HttpMethod.GET, null, new ParameterizedTypeReference<CustomPage<Profile>>() {});
-        CustomPage<Profile> customPage = responseEntity.getBody();
+       CustomPage<Profile> customPage = responseEntity.getBody();
         return customPage;
 
     }
